6e9a882987668a17ec3e9d72c20ac77c7420e5fa
Diego Tavares <oi@diegotavares.com.br>
2023-03-05 17:39:49 -0300

    feat: add answers to challenge-23

--- challenge-23/challenge-23.js
+++ challenge-23/challenge-23.js
@@ -1,4 +1,6 @@
-/*
+(function (window, document) {
+  "use strict";
+  /*
 Vamos desenvolver mais um projeto. A ideia é fazer uma mini-calculadora.
 As regras são:
 
@@ -23,3 +25,94 @@ multiplicação (x), então no input deve aparecer "1+2x".
 input;
 - Ao pressionar o botão "CE", o input deve ficar zerado.
 */
+
+  const $calcVisor = document.querySelector('[data-js="calc"]');
+  const $calcBtnNumber = document.querySelectorAll('[data-js="btn-number"]');
+  const $allOperations = ["+", "-", "x", "÷"];
+
+  //botoes de operações
+  const $calcBtnOperAll = document.querySelectorAll('[data-js="btn-oper"]');
+  const $calcBtnEq = document.querySelector('[data-js="btn-oper-eq"]');
+  const $calcBtnCe = document.querySelector('[data-js="btn-oper-ce"]');
+
+  /*
+  A API DOM recebeu algumas atualizações, e uma delas foi o método `forEach`
+  para o objeto NodeList. Então não precisamos mais usar o
+  `Array.prototype.forEach` para transformar a NodeList em um array =)
+  */
+
+  // Adiciona evento para o array de botoes criado a partir da seleção dos
+  //elementos do DOM
+  $calcBtnNumber.forEach(function (btn) {
+    btn.addEventListener("click", handleClickBtnNumber, false);
+  });
+
+  //array btn operators
+  $calcBtnOperAll.forEach(function (btn) {
+    btn.addEventListener("click", handleClickBtnOperator, false);
+  });
+
+  //btn operator ce
+  $calcBtnCe.addEventListener("click", handleClickBtnCe, false);
+
+  //btn operator eq
+  $calcBtnEq.addEventListener("click", handleClickBtnEq, false);
+
+  function handleClickBtnNumber() {
+    $calcVisor.value += this.value;
+  }
+
+  function handleClickBtnCe() {
+    $calcVisor.value = 0;
+  }
+
+  function handleClickBtnOperator() {
+    $calcVisor.value = removeLastCharIfOperator($calcVisor.value);
+    $calcVisor.value += this.value;
+  }
+
+  function removeLastCharIfOperator(char) {
+    if (isLastCharOperator(char)) {
+      return char.slice(0, -1);
+    }
+    return char;
+  }
+
+  //Se o último caractere no input já for um símbolo de alguma
+  //operação, esse caractere deve ser substituído pelo último pressionado
+  function isLastCharOperator(char) {
+    let lastChar = char.split("").pop();
+    return $allOperations.some(function (operator) {
+      return operator === lastChar;
+    });
+  }
+
+  function handleClickBtnEq() {
+    let allCharsVisor;
+
+    $calcVisor.value = removeLastCharIfOperator($calcVisor.value);
+    allCharsVisor = $calcVisor.value.match(/\d+[+x÷-]?/g);
+    $calcVisor.value = allCharsVisor.reduce(function (accumulated, actual) {
+      let firstValue = accumulated.slice(0, -1);
+      let operator = accumulated.split("").pop();
+      let lastValue = removeLastCharIfOperator(actual);
+      let lastOperator = isLastCharOperator(actual)
+        ? actual.split("").pop()
+        : "";
+
+      switch (operator) {
+        case "+":
+          return Number(firstValue) + Number(lastValue) + lastOperator;
+
+        case "-":
+          return Number(firstValue) - Number(lastValue) + lastOperator;
+
+        case "x":
+          return Number(firstValue) * Number(lastValue) + lastOperator;
+
+        case "÷":
+          return Number(firstValue) / Number(lastValue) + lastOperator;
+      }
+    });
+  }
+})(window, document);
